91d1bc639b77afe255dbc48d6c9ac577d742e5e5
Michael Babin <info@micb.dev>
2022-11-11 09:15:17 -0500

    fixing key for cta map on hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import {Button} from './button'
 import styled from 'styled-components'
 
-export default function Hero() {
+const Hero = (props) => {
     const data = useStaticQuery(graphql`
     query {
       contentfulHero {
@@ -18,7 +18,6 @@ export default function Hero() {
           href
         }
         featured {
-            id
             gatsbyImageData
         }
       }
@@ -30,18 +29,18 @@ export default function Hero() {
   return (
     <section className="row">
         <div className="col-lg-6 d-flex flex-column align-items-start justify-content-center pe-lg-5 py-5 py-lg-0 my-5 my-lg-0">
-        {title && (<h1>{title}</h1>)}
-        {kicker &&(<p className="kicker py-4">{kicker.kicker}</p>)}    
-        {cta && cta.map((content) => (<Button key={id} to={content.href}>{content.text}</Button>))}    
+            {title && (<h1>{title}</h1>)}
+            {kicker &&(<p className="kicker py-4">{kicker.kicker}</p>)}    
+            {cta && cta.map((content) => (<Button key={content.id} to={content.href}>{content.text}</Button>))}    
         </div>
         <div className="col-lg-6">
-        {featured && (
-          <FeaturedImage
-            alt='featured'
-            image={getImage(featured.gatsbyImageData)}
-            // className={styles.aboutHeroImage}
-          />
-        )}
+            {featured && (
+            <FeaturedImage
+                alt='featured'
+                image={getImage(featured.gatsbyImageData)}
+                // className={styles.aboutHeroImage}
+            />
+            )}
         </div>
     </section>
   )
@@ -53,6 +52,6 @@ const FeaturedImage = styled(GatsbyImage)`
   width:calc(100%);
   max-height:700px;
   box-shadow: -50px -50px 0px rgba(57,82,102,0.25), 50px 50px 0px rgba(87,122,147,0.25);
-  
 `
+export default Hero
 
